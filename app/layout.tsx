83bbd65b378d0ec4a1c3c08e44d0998b8f80e364
Wenny Yustalim <wennyyustalim@gmail.com>
2024-06-29 16:33:06 -0700

    Change template to nextjs ai chatbot

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,27 +1,64 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import { MyRuntimeProvider } from "@/app/MyRuntimeProvider";
-import { cn } from "@/lib/utils";
+import { GeistSans } from 'geist/font/sans'
+import { GeistMono } from 'geist/font/mono'
 
-import "./globals.css";
+import '@/app/globals.css'
+import { cn } from '@/lib/utils'
+import { TailwindIndicator } from '@/components/tailwind-indicator'
+import { Providers } from '@/components/providers'
+import { Header } from '@/components/header'
+import { Toaster } from '@/components/ui/sonner'
 
-const inter = Inter({ subsets: ["latin"] });
+export const metadata = {
+  metadataBase: process.env.VERCEL_URL
+    ? new URL(`https://${process.env.VERCEL_URL}`)
+    : undefined,
+  title: {
+    default: 'Next.js AI Chatbot',
+    template: `%s - Next.js AI Chatbot`
+  },
+  description: 'An AI-powered chatbot template built with Next.js and Vercel.',
+  icons: {
+    icon: '/favicon.ico',
+    shortcut: '/favicon-16x16.png',
+    apple: '/apple-touch-icon.png'
+  }
+}
 
-export const metadata: Metadata = {
-  title: "pobi",
-  description: "news analyzer",
-};
+export const viewport = {
+  themeColor: [
+    { media: '(prefers-color-scheme: light)', color: 'white' },
+    { media: '(prefers-color-scheme: dark)', color: 'black' }
+  ]
+}
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+interface RootLayoutProps {
+  children: React.ReactNode
+}
+
+export default function RootLayout({ children }: RootLayoutProps) {
   return (
-    <MyRuntimeProvider>
-      <html lang="en" className="h-full">
-        <body className={cn(inter.className, "h-full")}>{children}</body>
-      </html>
-    </MyRuntimeProvider>
-  );
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={cn(
+          'font-sans antialiased',
+          GeistSans.variable,
+          GeistMono.variable
+        )}
+      >
+        <Toaster position="top-center" />
+        <Providers
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <div className="flex flex-col min-h-screen">
+            <Header />
+            <main className="flex flex-col flex-1 bg-muted/50">{children}</main>
+          </div>
+          <TailwindIndicator />
+        </Providers>
+      </body>
+    </html>
+  )
 }
